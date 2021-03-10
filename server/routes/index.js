c659ae323a7235e6ffc2c06abe8f772a7859f47e
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2021-03-10 09:48:01 +0100

    refactor: refactor ping route endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 
 const welcomeRoutes = require("./welcome")
+const pingRoutes = require("./ping")
 
 const router = express.Router()
 
 router.use("/", welcomeRoutes)
+router.use("/", pingRoutes)
 
 module.exports = router
