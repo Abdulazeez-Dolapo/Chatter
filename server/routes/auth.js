e1527afc1afbc01d9000d25e97b11cd9ba35be21
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2021-03-12 13:49:08 +0100

    feat: create endpoint to check cookie status

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,15 @@
 const express = require("express")
+
+const { authenticate } = require("../middlewares/auth")
+
 const { registerUser } = require("../controllers/auth/register")
 const { loginUser } = require("../controllers/auth/login")
+const { readCookie } = require("../controllers/auth/cookie")
 
 const authRouter = express.Router()
 
 authRouter.post("/auth/register", registerUser)
 authRouter.post("/auth/login", loginUser)
+authRouter.get("/auth/cookie-status", authenticate, readCookie)
 
 module.exports = authRouter
