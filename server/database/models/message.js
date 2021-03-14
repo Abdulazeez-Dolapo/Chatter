c11d848c0c96010afde98468ea2192d596114cd3
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2021-03-14 12:27:52 +0100

    chore: set up message table

---
+++ server/database/models/message.js
@@ -0,0 +1,38 @@
+"use strict"
+const { Model } = require("sequelize")
+
+module.exports = (sequelize, DataTypes) => {
+	class Message extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+			this.belongsTo(models.User, {
+				foreignKey: "senderId",
+				as: "sender",
+			})
+
+			this.belongsTo(models.User, {
+				foreignKey: "receiverId",
+				as: "receiver",
+			})
+		}
+	}
+
+	Message.init(
+		{
+			senderId: DataTypes.INTEGER,
+			receiverId: DataTypes.INTEGER,
+			content: DataTypes.STRING,
+		},
+		{
+			sequelize,
+			modelName: "Message",
+		}
+	)
+
+	return Message
+}
