5dc4c6da0ff7775ad8e1c58dcb3ad37ac533df80
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2021-03-14 14:25:55 +0100

    chore: setup message and conversation tables

--- server/database/migrations/20210314124638-create-message.js
+++ server/database/migrations/20210314124638-create-message.js
@@ -1,18 +1,14 @@
-"use strict"
+'use strict';
 module.exports = {
 	up: async (queryInterface, Sequelize) => {
-		await queryInterface.createTable("Messages", {
+		await queryInterface.createTable('Messages', {
 			id: {
 				allowNull: false,
 				autoIncrement: true,
 				primaryKey: true,
-				type: Sequelize.INTEGER,
+				type: Sequelize.INTEGER
 			},
-			senderId: {
-				type: Sequelize.INTEGER,
-				allowNull: false,
-			},
-			receiverId: {
+			conversationId: {
 				type: Sequelize.INTEGER,
 				allowNull: false,
 			},
@@ -22,15 +18,16 @@ module.exports = {
 			},
 			createdAt: {
 				allowNull: false,
-				type: Sequelize.DATE,
+				type: Sequelize.DATE
 			},
 			updatedAt: {
 				allowNull: false,
-				type: Sequelize.DATE,
-			},
+				type: Sequelize.DATE
+			}
 		})
 	},
+
 	down: async (queryInterface, Sequelize) => {
-		await queryInterface.dropTable("Messages")
-	},
+		await queryInterface.dropTable('Messages')
+	}
 }
