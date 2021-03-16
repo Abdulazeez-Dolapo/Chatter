13e1a2555fc956a726870899aabf6ba1bdaad1e2
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2021-03-16 16:59:01 +0100

    feat: add hook to Message model update lastMessageId of conversations table

--- server/database/models/message.js
+++ server/database/models/message.js
@@ -33,5 +33,10 @@ module.exports = (sequelize, DataTypes) => {
 		indexes: [{ fields: 'createdAt' }]
 	})
 
+	Message.afterCreate((message) => {
+		const { id, conversationId } = message
+		sequelize.models.Conversation.update({ lastMessageId: id }, { where: { id: conversationId }})
+	})
+
 	return Message
 };
