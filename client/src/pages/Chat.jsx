09bbfe28fc5bb4cd80d384f3818e9f83874bdf32
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2021-04-02 23:16:32 +0100

    feat: display real user's name on profile On the ProfileDisplay component

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,3 +1,5 @@
+import { useContext } from "react"
+
 import Grid from "@material-ui/core/Grid"
 import { makeStyles } from "@material-ui/core/styles"
 
@@ -5,17 +7,21 @@ import ProfileDisplay from "../components/User/ProfileDisplay"
 import ContactList from "../components/Chat/ContactList"
 import ChatArea from "../components/Chat/ChatArea"
 
+import AuthContext from "../context/AuthContext"
+
 import chatPageStyles from "../styles/chat/chatPage"
+
 const useStyles = makeStyles(chatPageStyles)
 
 const Chat = () => {
 	const classes = useStyles()
+	const { user } = useContext(AuthContext)
 
 	return (
 		<Grid container className={classes.root}>
 			<Grid item xs={12} sm={4}>
 				<Grid className={classes.profile}>
-					<ProfileDisplay />
+					<ProfileDisplay name={user?.username} />
 				</Grid>
 
 				<ContactList />
