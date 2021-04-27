4aaad42db31741b52f2e4cb0f8a22dd664524727
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2021-04-27 10:10:38 +0100

    chore: comment out cors handler and use cors package An attempt to fix Netlify Cors error issue

--- server/app.js
+++ server/app.js
@@ -27,17 +27,17 @@ app.use(cors(corsOptions))
 app.use("/api", appRoutes)
 
 // Cors handler
-app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL)
-	res.header("Access-Control-Allow-Credentials", true)
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-WIth, Content-Type, Accept"
-	)
-	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
+// app.use(function (req, res, next) {
+// 	res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL)
+// 	res.header("Access-Control-Allow-Credentials", true)
+// 	res.header(
+// 		"Access-Control-Allow-Headers",
+// 		"Origin, X-Requested-WIth, Content-Type, Accept"
+// 	)
+// 	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
 
-	next()
-})
+// 	next()
+// })
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
