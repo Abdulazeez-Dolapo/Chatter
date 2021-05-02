4f8faf3c07c50e347e9fc04b9c9fd7f31795708c
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2021-05-02 09:04:01 +0100

    feat: add imageUrl to cookie status check response

--- server/controllers/auth/cookie.js
+++ server/controllers/auth/cookie.js
@@ -2,7 +2,7 @@ const readCookie = async (req, res, next) => {
 	try {
 		return res.status(200).json({
 			confirmed: true,
-			user: { username: req.user.username, id: req.user.id },
+			user: { username: req.user.username, id: req.user.id , imageUrl: req.user.imageUrl },
 		})
 	} catch (error) {
 		return next(createError(500))
