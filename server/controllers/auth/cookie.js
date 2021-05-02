fbc3e145bd5ce66df1a2f773e0d6365d11035517
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2021-05-02 09:59:54 +0100

    feat: add logout endpoint

--- server/controllers/auth/cookie.js
+++ server/controllers/auth/cookie.js
@@ -1,3 +1,6 @@
+const { cookieOptions } = require('../../utils/constants')
+const createError = require("http-errors")
+
 const readCookie = async (req, res, next) => {
 	try {
 		return res.status(200).json({
@@ -9,4 +12,18 @@ const readCookie = async (req, res, next) => {
 	}
 }
 
-module.exports = { readCookie }
+const logout = async (req, res, next) => {
+	try {
+		const { token } = req.cookies
+
+		res.cookie("token", token, { ...cookieOptions, maxAge: 1 })
+		return res.status(200).json({
+			message: "Logged out successfully",
+		})
+	} catch (error) {
+		console.log(error)
+		return next(createError(500))
+	}
+}
+
+module.exports = { readCookie, logout }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,12 +4,13 @@ const { authenticate } = require("../middlewares/auth")
 
 const { registerUser } = require("../controllers/auth/register")
 const { loginUser } = require("../controllers/auth/login")
-const { readCookie } = require("../controllers/auth/cookie")
+const { readCookie, logout } = require("../controllers/auth/cookie")
 
 const authRouter = express.Router()
 
 authRouter.post("/auth/register", registerUser)
 authRouter.post("/auth/login", loginUser)
 authRouter.get("/auth/cookie-status", authenticate, readCookie)
+authRouter.get("/auth/logout", logout)
 
 module.exports = authRouter
